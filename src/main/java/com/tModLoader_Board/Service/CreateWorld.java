92ae19b618bb436b58c82ab621cb5f30ef294951
lmymm <lmymm267525@gmail.com>
2025-09-04 16:40:44 +0800

    1.2

--- src/main/java/com/tModLoader_Board/Service/CreateWorld.java
+++ src/main/java/com/tModLoader_Board/Service/CreateWorld.java
@@ -84,7 +84,7 @@ public class CreateWorld {
      * 第二步：发送配置指令。
      * 由 /create/worldconfig 端点调用。
      */
-    public void sendCommand(String command) throws IOException {
+    public synchronized void sendCommand(String command) throws IOException {
         System.out.println("!!!");
         if (processWriter == null) {
             throw new IOException("进程未运行或未准备好接收指令。");
